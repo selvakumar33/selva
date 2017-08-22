608612639d9c9792cbaa175a2dfab0dbd8bb1bb3
selvakumar33 <30343633+selvakumar33@users.noreply.github.com>
2017-08-22 15:54:45 +0530

    Create s.c

---
+++ s.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a:
+printf("the number is"\n);
+scanf("%d",&a);
+if(a>0)
+{
+printf("the number is postive");
+}
+else
+{
+printf("the number is negative");
+}
+else if(a=0)
+{
+printf("the number is zero");
+}
+}
