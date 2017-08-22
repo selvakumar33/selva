31cb00c47c2aeb42bca229d130063cd8777ffa97
selvakumar33 <30343633+selvakumar33@users.noreply.github.com>
2017-08-22 16:21:27 +0530

    Create e.c

---
+++ e.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int ch;
+printf("the letter is\ch");
+scanf("c",&ch);
+if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O"||ch=='u'||ch=='U')
+{
+printf("the letter is vowel");
+}
+else
+{
+printf("the letter is not vowel");
+}
+}
